306ef68009500fcf514cf9dab54673808aa509ee
tushar13055 <tusharpatil13055@gmail.com>
2022-09-11 00:44:06 +0530

    practice

---
+++ test3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+    { 
+    int a =1000000L;
+    for (a= 1 ; a < 10; ++a);
+    
+    {
+      printf("%d, ",a);
+        /* code */
+    }
+    
+    }
---
+++ test4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+    { 
+   char ch;
+    //printf("%d",ch);
+    while (ch<256)
+    {
+        printf("%d, ",ch);
+        /* code */
+    }
+    return 0;
+    }
